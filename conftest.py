86005d5b6e15f10d459756a12f36758bf95392cc
Kanthi <kanthipavan1992@gmail.com>
2026-02-22 15:40:46 +0530

    added requirements file

--- conftest.py
+++ conftest.py
@@ -8,12 +8,20 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.support.wait import WebDriverWait
 from webdriver_manager.chrome import ChromeDriverManager
-
+from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope="function")
 def Launch_browser():
-    #service = Service(ChromeDriverManager().install())
-    browser = webdriver.Chrome()
+    chrome_options = Options()
+    chrome_options.add_argument("--headless=new")  # REQUIRED for Jenkins
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_options.add_argument("--window-size=1920,1080")
+
+    service = Service(ChromeDriverManager().install())
+
+    browser = webdriver.Chrome(service=service,options=chrome_options)
+
     config= ConfigParser()
     config.read("config.ini")
     wait = WebDriverWait(browser, 20)
