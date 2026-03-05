4b320c727d48baa658681658ca9c776d6edd5abe
Pavan431 <37466039+Pavan431@users.noreply.github.com>
2026-03-05 12:28:12 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -13,7 +13,14 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope="function")
 def Launch_browser():
     chrome_options = Options()
-    chrome_options.add_argument("--start-maximized")
+    #chrome_options.add_argument("--start-maximized")
+    #chrome_options.add_argument("--disable-notifications")
+    #chrome_options.add_argument("--disable-infobars")
+    #chrome_options.add_argument("--window-size=1920,1080")
+
+    chrome_options.add_argument("--headless=new")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
     chrome_options.add_argument("--disable-notifications")
     chrome_options.add_argument("--disable-infobars")
     chrome_options.add_argument("--window-size=1920,1080")
